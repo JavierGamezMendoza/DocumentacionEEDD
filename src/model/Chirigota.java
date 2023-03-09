4fd18d9bc696ec31e943a26618e4184f22b631d0
Habisito <javigm1012@gmail.com>
2023-03-09 22:36:32 +0100

    Comentada la clase Chirigota

--- src/model/Chirigota.java
+++ src/model/Chirigota.java
@@ -1,8 +1,23 @@
 package model;
 
+/**
+ * Clase para las chirigotas
+ * @author Javier Gámez Mendoza
+ * @version 1.0
+ */
 public class Chirigota extends AgrupacionOficial implements Callejera{
     private Integer nCuples;
 
+    /**
+     * Constructor de las chirigotas
+     * @param nombre Nombre de la chirigota
+     * @param autor Autor de la chirigota
+     * @param autorMusica Autor de la música de la chirigota
+     * @param autorLetra Auto de la letra de la chirigota
+     * @param tipo Tipo de chirigota
+     * @param nCuples Número de Cuples de la chirigota
+     * @param pts Número de puntos de la chirigota
+     */
     public Chirigota(String nombre, String autor, String autorMusica, String autorLetra, String tipo, Integer nCuples, Integer pts) {
         super(nombre, autor, autorMusica, autorLetra, tipo, pts);
         this.setnCuples(nCuples);
@@ -17,24 +32,31 @@ public class Chirigota extends AgrupacionOficial implements Callejera{
     }
 
 
+    /**
+     * Función que presenta una chirigota
+     * @return Retorna una cadena de texto con la presentación de la chirigota
+     */
     @Override
     public String cantarPresentacion() {
         return "Cantando la presentación de la Chirigota " + getNombre();
     }
 
+    /**
+     * Función que anuncia que una chirigota va caminito del falla
+     * @return Retorna una cadena de texto que anuncia que una chirigota va caminito del falla
+     */
     @Override
     public String caminitoDelFalla() {
         return "Chirigota " + getNombre() + " caminito del Falla";
     }
 
-    @Override
-    public String hacerTipo() {
-        return "Amo a escuchar la chirigota " + getNombre();
-    }
-
+    /**
+     * Funcion que presenta la actuación de una chirigota
+     * @return Retorna una cadena de texto con el anuncio de la chirigota
+     */
     @Override
     public String amoAescucha() {
-        return "Chirigota " + this + ", cuples=" + getnCuples();
+        return "Amo a escuchar la chirigota " + getNombre();
     }
 }
 
