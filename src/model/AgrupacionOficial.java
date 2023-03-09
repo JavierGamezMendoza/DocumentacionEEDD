f29bb641632490809352ac95320c333f970d573e
Habisito <javigm1012@gmail.com>
2023-03-09 22:25:16 +0100

    Comentada la clase Agrupación Oficial y la interfaz Callejera

--- src/model/AgrupacionOficial.java
+++ src/model/AgrupacionOficial.java
@@ -1,11 +1,26 @@
 package model;
 
+
+/**
+ * Clase para Agrupaciones oficiales
+ * @author Javier Gámez Mendoza
+ * @version 1.0
+ */
 public abstract class AgrupacionOficial extends Agrupacion{
     private final Integer MAX = 10;
     private Integer pts;
     private Integrante[] integrantes;
     private Integer iIntegrantes = 0;
 
+    /**
+     * Constructor para Agrupaciones oficiales
+     * @param nombre Nombre de la agrupación oficial
+     * @param autor Autor de la agrupación oficial
+     * @param autorMusica Autor de la música de la agrupación oficial
+     * @param autorLetra Autor de la letra de la agrupación oficial
+     * @param tipo Tipo de agrupación oficial
+     * @param pts Puntos de la agrupación oficial
+     */
     public AgrupacionOficial(String nombre, String autor, String autorMusica, String autorLetra, String tipo, Integer pts) {
         super(nombre, autor, autorMusica, autorLetra, tipo);
         integrantes = new Integrante[MAX];
@@ -49,13 +64,16 @@ public abstract class AgrupacionOficial extends Agrupacion{
 
     private Integer puntos;
 
-
+    /**
+     * Funcion que anuncia que una agrupación va caminito del falla
+     * @return Retorna una cadena de texto anunciando que la agrupación va caminito del falla
+     */
     public abstract String caminitoDelFalla();
 
+    /**
+     * Funcion que canta la presentación de una agrupación
+     * @return Retorna una cadena de texto con la presentación de la agrupación
+     */
     public abstract String cantarPresentacion();
 
-    @Override
-    public String hacerTipo() {
-        return null;
-    }
 }
--- src/model/Callejera.java
+++ src/model/Callejera.java
@@ -1,5 +1,15 @@
 package model;
 
+/**
+ * Interfaz para agrupaciones callejeras
+ * @author Javier Gámez Mendoza
+ * @version 1.0
+ */
 public interface Callejera {
+    /**
+     * Función que anuncia la actuación de una agrupación
+     * @return Retorna una cadena de texto que anuncia la actuación de una agrupación
+     */
     public String amoAescucha();
+
 }
